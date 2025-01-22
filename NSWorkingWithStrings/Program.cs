51e79a922ef844b6f178b55a646126e3043b9500
Zlodeyanin <evgeniy_zavarnitsin@mail.ru>
2025-01-22 17:34:28 +0300

    Добавьте файлы проекта.

---
+++ NSWorkingWithStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NSWorkingWithStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, zodiacSign, workplace;
+            int age;
+            Console.Write("Ваше имя: ");
+            name = Console.ReadLine();
+            Console.Write("Ваш возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ваш знак зодиака: ");
+            zodiacSign = Console.ReadLine();
+            Console.Write("Ваше место работы: ");
+            workplace = Console.ReadLine();
+            Console.WriteLine($"Ваше имя - {name}, ваш возраст - {age}, ваш знак зодиака - {zodiacSign}, ваше место работы - {workplace}");
+            Console.ReadKey();
+        }
+    }
+}
